0795448a30849ae1d2c21f3984303ff4ae42b24c
Sluengo <stevenluengo@gmail.com>
2021-11-07 11:23:18 -0500

    ADding code to app

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
+def index():
+   return 'Main Page: This app is running in docker on AWS EKS.'
+   
+@app.route('/hello')
 def hello_world():
-   return "Hello, World! This app is running in a docker container hosted on an AWS EKS Cluster. Nice!"
+   return "Hello, World! This is another page!"
+
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000)
